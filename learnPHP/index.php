d2bfd25568d458ecf1366b641871995a454cabd9
michellemwangi01 <michellemwangim@gmail.com>
2023-03-01 12:43:21 +0300

    first commit

---
+++ learnPHP/index.php
@@ -0,0 +1,24 @@
+<?php
+$name = "Amirah"; //variable declaration
+define ('NAME', "Coaral" ); //constant declaration
+$myArray = [12,12,23,45]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    
+    <h1><?php echo "Welcome $name"  ?> </h1>
+    <h1><?php echo  NAME  ?> </h1>
+
+
+   
+    </div>
+</body>
+</html>
